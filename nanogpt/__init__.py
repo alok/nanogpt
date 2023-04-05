4b7adc0ef704fc9a59811b422be9ab62ca56d010
Alok Singh <alokbeniwal@gmail.com>
2023-04-04 17:59:25 -0700

    encode/decode text
    Co-authored-by: Megan Crawford <spiracular@users.noreply.github.com>

--- nanogpt/__init__.py
+++ nanogpt/__init__.py
@@ -1,14 +1,56 @@
 # %%
-
+import random
+import string
+from typing import Final
 from pathlib import Path
 import urllib.request
+import torch
+import bidict
 
 INPUT_FILE = Path("input.txt")
 data = urllib.request.urlretrieve(
     url="https://raw.githubusercontent.com/karpathy/char-rnn/master/data/tinyshakespeare/input.txt",
     filename=INPUT_FILE,
 )
-data = INPUT_FILE.read_text()
+raw_data = INPUT_FILE.read_text()
 
-chars = sorted(set(data))
-text = ''.join(chars)
+chars = sorted(set(raw_data))
+text = "".join(chars)
+# %% Tokenization
+
+
+# encoder: str -> [int]
+# decoder: [int] -> str
+# init codebook/vocab
+
+VOCAB: Final[bidict.bidict[int, str]] = bidict.bidict(enumerate(chars))
+
+
+# %%
+def encode(s: str) -> list[int]:
+    return [VOCAB.inv[c] for c in s]
+
+
+# %%
+def decode(l: list[int]) -> str:
+    return "".join([VOCAB[i] for i in l])
+
+
+# %%
+s = "".join(random.choices(string.ascii_lowercase, k=10))
+assert decode(encode(s)) == s
+assert encode("abc") == [39, 40, 41]
+assert decode([41, 40, 39]) == "cba"
+
+# %% Encode dataset and store in a torch tensor
+data = torch.tensor(encode(raw_data), dtype=torch.long)  #
+
+
+# %% Split into train and test sets
+# rolling lookback
+
+WINDOW_SIZE: int = 5  # v important that constants be in SCREAMING_SNAKE_CASE
+random.seed
+
+split = 0.9 * len(data)
+train, val = data[:split], data[split:]
